7824d5f083a2fbb3a43b8c56f71388adae85520e
Aulia Amirullah <auliaamir153@gmail.com>
2024-12-15 20:26:16 +0000

    Added the mobile menus

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,6 +2,15 @@
   <div class="container">
     <input id="click" name="exit" type="checkbox" @click="menuToggle" />
     <label for="click"><span class="burger"></span></label>
+
+    <div class="fullscreen-menu" v-if="menuOpened" @click="closeMenu">
+      <ul class="menu-list">
+        <li><a href="#section1">Home</a></li>
+        <li><a href="#section2">About</a></li>
+        <li><a href="#section3">Services</a></li>
+        <li><a href="#section4">Contact</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -21,6 +30,9 @@ export default {
   methods: {
     menuToggle() {
       this.menuOpened = !this.menuOpened;
+    },
+    closeMenu() {
+      this.menuOpened = false;
     },
   },
 };
@@ -48,78 +60,6 @@ export default {
   background: transparent;
 }
 
-.container label:before,
-.container input:checked + label:before {
-  content: "";
-  position: absolute;
-  top: 50%;
-  margin-top: -2px;
-  width: 30px;
-  height: 2px;
-  border-radius: 2px;
-  background: #42454a;
-}
-
-.container label:before {
-  -webkit-animation: animationOneReverse 1s ease forwards;
-  animation: animationOneReverse 1s ease forwards;
-}
-
-@-webkit-keyframes animationOneReverse {
-  0% {
-    -webkit-transform: rotate(315deg);
-  }
-  25% {
-    -webkit-transform: rotate(360deg);
-  }
-  50%,
-  100% {
-    -webkit-transform: rotate(0deg);
-  }
-}
-@keyframes animationOneReverse {
-  0% {
-    transform: rotate(315deg);
-  }
-  25% {
-    transform: rotate(360deg);
-  }
-  50%,
-  100% {
-    transform: rotate(0deg);
-  }
-}
-
-.container input:checked + label:before {
-  -webkit-animation: animationOne 1s ease forwards;
-  animation: animationOne 1s ease forwards;
-}
-
-@-webkit-keyframes animationOne {
-  0%,
-  50% {
-    -webkit-transform: rotate(0deg);
-  }
-  75% {
-    -webkit-transform: rotate(360deg);
-  }
-  100% {
-    -webkit-transform: rotate(315deg);
-  }
-}
-@keyframes animationOne {
-  0%,
-  50% {
-    transform: rotate(0deg);
-  }
-  75% {
-    transform: rotate(360deg);
-  }
-  100% {
-    transform: rotate(315deg);
-  }
-}
-
 .container label:after,
 .container input:checked + label:after {
   content: "";
@@ -132,68 +72,6 @@ export default {
   background: #42454a;
 }
 
-.container label:after {
-  -webkit-animation: animationTwoReverse 1s ease forwards;
-  animation: animationTwoReverse 1s ease forwards;
-}
-
-@-webkit-keyframes animationTwoReverse {
-  0% {
-    -webkit-transform: rotate(405deg);
-  }
-  25% {
-    -webkit-transform: rotate(450deg);
-  }
-  50%,
-  100% {
-    -webkit-transform: rotate(0deg);
-  }
-}
-@keyframes animationTwoReverse {
-  0% {
-    transform: rotate(405deg);
-  }
-  25% {
-    transform: rotate(450deg);
-  }
-  50%,
-  100% {
-    transform: rotate(0deg);
-  }
-}
-
-.container input:checked + label:after {
-  -webkit-animation: animationTwo 1s ease forwards;
-  animation: animationTwo 1s ease forwards;
-}
-
-@-webkit-keyframes animationTwo {
-  0%,
-  50% {
-    -webkit-transform: rotate(0deg);
-  }
-  75% {
-    -webkit-transform: rotate(450deg);
-  }
-  100% {
-    -webkit-transform: rotate(405deg);
-  }
-}
-@keyframes animationTwo {
-  0%,
-  50% {
-    transform: rotate(0deg);
-  }
-  75% {
-    transform: rotate(450deg);
-  }
-  100% {
-    transform: rotate(405deg);
-  }
-}
-
-/* Burger Icon */
-
 .container label .burger:before {
   content: "";
   position: absolute;
@@ -203,75 +81,6 @@ export default {
   height: 2px;
   border-radius: 2px;
   background: #42454a;
-  -webkit-animation: animationBurgerTopReverse 1s ease forwards;
-  animation: animationBurgerTopReverse 1s ease forwards;
-}
-
-@-webkit-keyframes animationBurgerTopReverse {
-  0%,
-  50% {
-    -webkit-transform: translateY(12px);
-    opacity: 0;
-  }
-  51% {
-    -webkit-transform: translateY(12px);
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: translateY(0px);
-    opacity: 1;
-  }
-}
-@keyframes animationBurgerTopReverse {
-  0%,
-  50% {
-    transform: translateY(12px);
-    opacity: 0;
-  }
-  51% {
-    transform: translateY(12px);
-    opacity: 1;
-  }
-  100% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-}
-
-.container input:checked + label .burger:before {
-  -webkit-animation: animationBurgerTop 1s ease forwards;
-  animation: animationBurgerTop 1s ease forwards;
-}
-
-@-webkit-keyframes animationBurgerTop {
-  0% {
-    -webkit-transform: translateY(0px);
-    opacity: 1;
-  }
-  50% {
-    -webkit-transform: translateY(12px);
-    opacity: 1;
-  }
-  51%,
-  100% {
-    -webkit-transform: translateY(12px);
-    opacity: 0;
-  }
-}
-@keyframes animationBurgerTop {
-  0% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-  50% {
-    transform: translateY(12px);
-    opacity: 1;
-  }
-  51%,
-  100% {
-    transform: translateY(12px);
-    opacity: 0;
-  }
 }
 
 .container label .burger:after {
@@ -282,84 +91,39 @@ export default {
   height: 2px;
   border-radius: 2px;
   background: #42454a;
-  -webkit-animation: animationBurgerBottomReverse 1s ease forwards;
-  animation: animationBurgerBottomReverse 1s ease forwards;
 }
 
-@-webkit-keyframes animationBurgerBottomReverse {
-  0%,
-  50% {
-    -webkit-transform: translateY(-12px);
-    opacity: 0;
-  }
-  51% {
-    -webkit-transform: translateY(-12px);
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: translateY(0px);
-    opacity: 1;
-  }
-}
-@keyframes animationBurgerBottomReverse {
-  0%,
-  50% {
-    transform: translateY(-12px);
-    opacity: 0;
-  }
-  51% {
-    transform: translateY(-12px);
-    opacity: 1;
-  }
-  100% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
+.fullscreen-menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.9);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
 }
 
-.container input:checked + label .burger:after {
-  -webkit-animation: animationBurgerBottom 1s ease forwards;
-  animation: animationBurgerBottom 1s ease forwards;
+.menu-list {
+  list-style: none;
+  padding: 0;
+  text-align: center;
 }
 
-@-webkit-keyframes animationBurgerBottom {
-  0% {
-    -webkit-transform: translateY(0px);
-    opacity: 1;
-  }
-  50% {
-    -webkit-transform: translateY(-12px);
-    opacity: 1;
-  }
-  51%,
-  100% {
-    -webkit-transform: translateY(-12px);
-    opacity: 0;
-  }
-}
-@keyframes animationBurgerBottom {
-  0% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-  50% {
-    transform: translateY(-12px);
-    opacity: 1;
-  }
-  51%,
-  100% {
-    transform: translateY(-12px);
-    opacity: 0;
-  }
+.menu-list li {
+  margin: 20px 0;
 }
 
-@media (max-width: 992px) {
-  span.menu {
-    font-size: 14px;
-  }
+.menu-list li a {
+  text-decoration: none;
+  font-size: 24px;
+  color: #ffffff;
+  transition: color 0.3s ease;
+}
 
-  .container label {
-    height: 26px;
-  }
+.menu-list li a:hover {
+  color: #ff6f61;
 }
 </style>
